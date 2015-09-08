4dce9cb21220eff3f4b8a31855e4c2286b88d2a6
alexvanacker <alexvanacker@gmail.com>
2015-09-08 18:50:04 +0200

    Extension code asks content script for URLs and gets them!
    Need to:
    
     * setup Drive authentication
     * download the tickets

--- popup.js
+++ popup.js
@@ -1,11 +1,35 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log("Getting URLs from page!");
-    console.log(request);
-    for (var i = request.length - 1; i >= 0; i--) {
-      newURL = request[i];
-      chrome.tabs.create({ url: newURL });
-    };
-  });
+/**
+ * Given a URL to a ticket, saves the ticket to the user's Google Drive
+ */
+function saveTicketToDrive(url){
+
+}
+
+/**
+ * Setup connection to user's Drive
+ */
+function oauthToDrive(){
+
+}
+
+function onLoadFunction(){
+  var downloadButton = document.getElementById('geturl');
+  if(downloadButton != null){
+    downloadButton.addEventListener('click', function(){
+      //TODO  Send request for all URLS
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {query: "urls"}, function(response) {
+          var urls = response;
+          console.log(urls);
+        });
+      });
+      
+    });  
+  }else{
+    console.log("ERROR: cannot find the download button");
+  }
+};
+
+document.onload = onLoadFunction();
 
 
--- urlextract.js
+++ urlextract.js
@@ -1,3 +1,8 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+      if (request.query == "urls"){
+         sendResponse(getAllTicketPrintURLS());
+      }
+});
 
 function getAllTicketPrintURLS(){
     listPrintContainers = $(".print_button_container_for_expand_panel");
@@ -10,16 +15,13 @@ function getAllTicketPrintURLS(){
         // Absolute URL
         listUrls.push(window.location.origin  + $(this).attr('href'));
     });
-    console.log(listUrls);
     return listUrls;
 };
 
 function executeScript(){
-    console.log("Running awesome script");
-    var getURLs = getAllTicketPrintURLS();
-    chrome.extension.sendMessage(getAllTicketPrintURLS(), function(anyResponse){
-        return "OK";
-    });
+    // chrome.extension.sendMessage(getAllTicketPrintURLS(), function(anyResponse){
+    //     return "OK";
+    // });
     // Send to popup 
 }
 
